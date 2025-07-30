83759b352f73ba5f5c3dcf73f09740b4e585e168
charles <charlesgintoki@gmail.com>
2025-07-30 16:42:57 +0800

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src="" alt="" />
+      
+      <ul>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Portpolio</li>
+        <li>Contact Me</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
