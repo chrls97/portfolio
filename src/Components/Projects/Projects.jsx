44a6f337286a4e3606320cb84dba794b537ed28d
charles <charlesgintoki@gmail.com>
2025-08-04 13:20:01 +0800

    added projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -10,11 +10,29 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import html from '../../Assets/stack_icons/html.png'
 import css from '../../Assets/stack_icons/css.png'
 import js from '../../Assets/stack_icons/js.png'
+import react from '../../Assets/stack_icons/react.png'
 import bootstrap from '../../Assets/stack_icons/bootstrap.png'
+import tailwind from '../../Assets/stack_icons/tailwind.png'
 import php from '../../Assets/stack_icons/php.png'
 import mysql from '../../Assets/stack_icons/mysql.png'
+import laravel from '../../Assets/stack_icons/laravel.png'
+import nodejs from '../../Assets/stack_icons/nodejs.png'
+import expressjs from '../../Assets/stack_icons/expressjs.png'
+import mongodb from '../../Assets/stack_icons/mongodb.png'
+import vscode from '../../Assets/stack_icons/vscode.png'
+import git from '../../Assets/stack_icons/git.png'
+import github from '../../Assets/stack_icons/github.png'
+import postman from '../../Assets/stack_icons/postman.png'
+import npm from '../../Assets/stack_icons/npm.png'
+import photoshop from '../../Assets/stack_icons/photoshop.png'
 
 import charles from '../../assets/charles.jpg'
+import isii from '../../assets/system_ss/isi-website.png'
+import cslauncher from '../../assets/system_ss/cslauncher.png'
+import misOperations from '../../assets/system_ss/mis-operations.png'
+import upc from '../../assets/system_ss/upc-website.png'
+import eeg from '../../assets/system_ss/eeg-website.png'
+
 import { div, hr } from 'framer-motion/client'
 
 const Projects = () => {
@@ -26,7 +44,7 @@ const Projects = () => {
   const projects = [{
     title: 'iSolutions Operations System',
     description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ad commodi voluptates harum eos delectus, molestias totam possimus suscipit doloribus nisi labore maiores, maxime sint nihil exercitationem eum! Eius, beatae!',
-    image: charles,
+    image: isii,
     toolsUsed: [html, css, js, bootstrap, php, mysql],
     liveView: true,
     liveViewLink: '#',
@@ -35,19 +53,46 @@ const Projects = () => {
   },{
     title: 'iSolutions Website',
     description: 'Lorem ipsum does, maxime sint nihil exercitationem eum! Eius, beatae!',
-    image: charles,
+    image: isii,
     toolsUsed: [html, css, js, bootstrap, php, mysql],
     liveView: true,
     liveViewLink: '#',
     github: true,
     githubLink: '#'
   },{
-    title: 'iSolutions Website',
+    title: 'United Portusers Confederation Website',
     description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ad commodi voluptates harum eos delectus, molestias totam possimus suscipit doloribus nisi labore maiores, maxime sint nihil exercitationem eum! Eius, beatae!',
-    image: charles,
+    image: upc,
     toolsUsed: [html, css, js, bootstrap, php, mysql],
     liveView: true,
     liveViewLink: '#',
+    github: true,
+    githubLink: '#'
+  },{
+    title: 'Eagle Express Lines Website',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ad commodi voluptates harum eos delectus, molestias totam possimus suscipit doloribus nisi labore maiores, maxime sint nihil exercitationem eum! Eius, beatae!',
+    image: eeg,
+    toolsUsed: [html, css, js, bootstrap, php, mysql],
+    liveView: true,
+    liveViewLink: '#',
+    github: true,
+    githubLink: '#'
+  },{
+    title: 'MIS Operations System',
+    description: 'Lorem ipsum does, maxime sint nihil exercitationem eum! Eius, beatae!',
+    image: misOperations,
+    toolsUsed: [html, css, js, bootstrap, php, mysql],
+    liveView: true,
+    liveViewLink: 'http://www.cargosoft.com.ph/mis-operations/index.php',
+    github: true,
+    githubLink: '#'
+  },{
+    title: 'Cargosoft Launcher System',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ad commodi voluptates harum eos delectus, molestias totam possimus suscipit doloribus nisi labore maiores, maxime sint nihil exercitationem eum! Eius, beatae!',
+    image: cslauncher,
+    toolsUsed: [html, css, js, bootstrap, php, mysql],
+    liveView: true,
+    liveViewLink: 'http://www.cargosoft.com.ph/cargosoft-launcher/',
     github: true,
     githubLink: '#'
   }];
@@ -77,14 +122,17 @@ const Projects = () => {
                       }
                     </div>
                     <div className='project-links'>
-                      {project.liveView ? <a href={project.liveViewLink}><FontAwesomeIcon icon={faGithub} />View Live</a> :''}
-                      {project.github ? <a href={project.githubLink}><FontAwesomeIcon icon={faGithub} /> Github</a> :''}
+                      {project.liveView ? <a href={project.liveViewLink} target='_blank'>View Live</a> :''}
+                      {project.github ? <a href={project.githubLink} target='_blank'><FontAwesomeIcon icon={faGithub} /> Github</a> :''}
                     </div>
                   </div>
                 </div>
               )
             })
           }
+          <div>
+            <button>See More</button>
+          </div>
         </div>
         
       </div>
