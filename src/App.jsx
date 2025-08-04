a467022d849c42ce5660b987e4f871189bba434f
charles <charlesgintoki@gmail.com>
2025-08-04 13:48:24 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import AboutMe from './Components/AboutMe/AboutMe'
 import Projects from './Components/Projects/Projects'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RouteLayout from './layout/RouteLayout'
 
 const App = () => {
+  const router = createBrowserRouter(createRoutesFromElements(
+    // Main layout with Navbar
+    <Route path='/' element={<RouteLayout/>}> 
+      <Route index element={ // Default route
+        <>
+          <Hero />
+          <AboutMe />
+          <Projects />
+        </>
+      } />
+    </Route>
+  ));
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <AboutMe />
-      <Projects />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Navbar.css'
+import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div className='navbar'>
       <h2>CHARLES <span>AMON</span></h2>
 
       <ul>
-        <li>Home</li>
-        <li><a href="#aboutme">About Me</a></li>
-        <li>Projects</li>
-        <li>Contact Me</li>
+         <NavLink to='/'><li >Home</li></NavLink>
+        <NavLink to='#aboutme'><li >About Me</li></NavLink>
+        <NavLink to='#project'><li >Projects</li></NavLink>
+        <NavLink to='#contact'><li >Contact</li></NavLink>
       </ul>
       
     </div>
