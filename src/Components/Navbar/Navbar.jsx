63a5614d2b1039c313fd22e982c4f5e3a1921e5b
charles <charlesgintoki@gmail.com>
2025-08-05 20:38:10 +0800

    navbar mobile view, added menu button for sidebar open

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+
 const Navbar = () => {
+
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0"
+  }
+
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px"
+  }
+
+  const handleNavClick = () => {
+    // Close menu only on mobile view (width <= 800px)
+    if (window.innerWidth <= 800) {
+      closeMenu();
+    }
+  };
+
   return (
     <div className='navbar'>
-      <h2>CHARLES <span>AMON</span></h2>
-
-      <ul>
-
-        <li><HashLink smooth to="/#hero">Home</HashLink></li>
-        <li><HashLink smooth to="/#aboutme">About Me</HashLink></li>
-        <li><HashLink smooth to="/#projects">Projects</HashLink></li>
-        <li><HashLink smooth to="/#contact">Contact Me</HashLink></li>
+      <HashLink smooth to="#hero"><h2>CHARLES <span>AMON</span></h2></HashLink>
+      <button className='nav-mob-open' onClick={openMenu}><FontAwesomeIcon icon={faBars} /></button>
+      <ul ref={menuRef} className='nav-menu'>
+        <button className='nav-mob-close' onClick={closeMenu}><FontAwesomeIcon icon={faXmark} /></button>
+        <li onClick={handleNavClick}><HashLink smooth to="/#hero">Home</HashLink></li>
+        <li onClick={handleNavClick}><HashLink smooth to="/#aboutme">About Me</HashLink></li>
+        <li onClick={handleNavClick}><HashLink smooth to="/#projects">Projects</HashLink></li>
+        <li onClick={handleNavClick}><HashLink smooth to="/#contact">Contact Me</HashLink></li>
       </ul>
     </div>
   );
