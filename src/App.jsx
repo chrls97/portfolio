636ba7045b6c8ede0af55349d992dc22c6d6a3f2
charles <charlesgintoki@gmail.com>
2025-08-20 00:22:24 +0800

    test21

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-<<<<<<< HEAD
 import Hero from './Components/Hero'
 import AboutMe from './Components/AboutMe'
-=======
-import Hero from './Components/Hero/Hero'
-import AboutMe from './Components/AboutMe/AboutMe'
->>>>>>> master
 import Projects from './Components/Projects/Projects'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import RouteLayout from './layout/RouteLayout'
 import AllProjects from './Components/AllProjects/AllProjects'
-<<<<<<< HEAD
 import Contact from './Components/Contact'
 import { ToastContainer } from 'react-toastify';
-=======
-import Contact from './Components/Contact/Contact'
 
->>>>>>> master
 
 const App = () => {
   const router = createBrowserRouter(createRoutesFromElements(
@@ -31,7 +22,7 @@ const App = () => {
         </>
       } />
       
-      <Route path='projects' element={<AllProjects />} />
+      <Route path='projects' element={<AllProjects />} />g
     </Route>
   ));
 
